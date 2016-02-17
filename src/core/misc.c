83736832428b3754b301be460fad28289701041f
LemonBoy <thatlemon@gmail.com>
2016-02-18 00:14:00 +0100

    Implement find_substr with strarray_find

--- src/core/misc.c
+++ src/core/misc.c
@@ -123,33 +123,6 @@ long get_timeval_diff(const GTimeVal *tv1, const GTimeVal *tv2)
 	return usecs;
 }
 
-int find_substr(const char *list, const char *item)
-{
-	const char *ptr;
-
-	g_return_val_if_fail(list != NULL, FALSE);
-	g_return_val_if_fail(item != NULL, FALSE);
-
-	if (*item == '\0')
-		return FALSE;
-
-	for (;;) {
-		while (i_isspace(*list)) list++;
-		if (*list == '\0') break;
-
-		ptr = strchr(list, ' ');
-		if (ptr == NULL) ptr = list+strlen(list);
-
-		if (g_ascii_strncasecmp(list, item, ptr-list) == 0 &&
-		    item[ptr-list] == '\0')
-			return TRUE;
-
-		list = ptr;
-	}
-
-	return FALSE;
-}
-
 int strarray_find(char **array, const char *item)
 {
 	char **tmp;
@@ -165,6 +138,24 @@ int strarray_find(char **array, const char *item)
 	}
 
 	return -1;
+}
+
+int find_substr(const char *list, const char *item)
+{
+	char **items;
+	int i;
+
+	g_return_val_if_fail(list != NULL, FALSE);
+	g_return_val_if_fail(item != NULL, FALSE);
+
+	items = g_strsplit(list, " ", -1);
+	if (items == NULL)
+		return FALSE;
+
+	i = strarray_find(items, item);
+	g_strfreev(items);
+
+	return (i != -1);
 }
 
 GSList *gslist_find_string(GSList *list, const char *key)
